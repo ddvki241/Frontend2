b8f84ac5ca958932ffc5628a71e1ac4978bc99ae
ddvki241 <dmitrivdenus@gmail.com>
2025-03-17 19:29:42 +0200

    Add files via upload

---
+++ lab_1/script.js
@@ -0,0 +1,59 @@
+//Task 1
+console.log("Task 1");
+let x=1;
+let y=2;
+let z=3;
+let result=((x-z)<=(z+Math.sqrt(x))<=(2*y));
+console.log(result);
+//Task 2
+console.log("Task 2");
+let a= -1.17;
+x= 3.4
+y=(a*(Math.cbrt(Math.pow(Math.sin(Math.pow(x,3)),4)))+12.47);
+let m=(Math.sqrt(Math.abs(x+a))+17.14*Math.log10(Math.PI/3));
+let t=(Math.log(Math.abs(m-y))+Math.pow(Math.cos(m*y),3));
+console.log("Result is: "+t);
+//Task 3
+console.log("Task 3");
+x=1;
+z=null;
+if(Math.abs(x*y)<=1||x<0){
+	(x+y)/(Math.exp(x*y));
+} else if(2<x||y<=0){
+	-Math.pow(Math.log(x),2);
+} else if(0<y||0<=x<=2){
+	Math.log10(Math.sqrt(y));
+}
+if (z===null){
+	console.log('Wrong input');
+} else{
+	console.log(z);
+}
+//Task 4
+console.log("Task 4.1");
+for(let t=0; t<=6.5; t+=1.1){
+	z=((2.3*t+8)/(Math.abs(2*Math.cos(t)))+1)
+	console.log(z);
+}
+console.log("Task 4.2");
+t>=0.4
+for(let i=0;i<7;i++){
+	z=((2.3*t+8)/(Math.abs(2*Math.cos(t)))+1)
+	t+=0.9;
+	console.log(z);
+}
+//Task 5
+console.log("Task 5.1");
+let S=0;
+i=4;
+k=11;
+for(let n=i; n<=k; n++){
+	S+=((n)/(Math.pow(n,2)+5*n+6))
+}
+console.log(S);
+console.log("Task 5.2");
+let P=1
+for(l=11; l<=17; l++){
+	P*=(Math.pow(-1,l))*(3*l-4)/(Math.pow(l,2)+7);
+}
+console.log(P);
